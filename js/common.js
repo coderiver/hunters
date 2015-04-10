3ed593a6ed0a5589719ff7800f9977b68e3947e9
etna-scorpio <etna.scorpio@gmail.com>
2015-04-10 18:28:53 +0300

    2 sections added

--- js/common.js
+++ js/common.js
@@ -1,10 +1,47 @@
 head.ready(function() {
 
-	$('.js-slick').slick({
+	//sliders
+	$('.main__slider .js-slick').slick({
 		dots: true,
-		slidesToShow: 1
+		slidesToShow: 1,
 	});
 
+	$('.photo__slider .js-slick').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+
+	$('.photo__albums .js-slick').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3
+	});
+
+	// function getData(){
+
+	// 	var album = $('.photo__albums').find('.slide');
+	// 	var slide = $('.photo__slider').find('.js-slick').children('img');
+
+	// 	console.log(slide);
+
+	// 	album.click(function () {
+
+	// 		var y = $(this).attr('data-tab');
+	// 		var x = slide.children().attr('data-number');
+
+	// 	// if(x == y){
+	// 	// 	slide.slick('slickFilter','data-number');
+	// 	// }
+
+	// 		console.log(x);
+	// 	});
+
+	// }
+
+	// getData();
+
+
+	// scroll for video section
 	$('.js-scroll').jScrollPane({
 		showArrows: true,
 		verticalDragMaxHeight: 20,
@@ -17,6 +54,7 @@ head.ready(function() {
 		});
 	});
 
+	//navigation menu
 	$('.mobile__nav').click(function (e) {
 		e.stopPropagation();
 		$('.navigation__menu').toggleClass('is-visible');
@@ -24,8 +62,46 @@ head.ready(function() {
 
 	$('body').click(function (e) {
 		if($('.navigation__menu').hasClass('is-visible')){
-			$('.navigation__menu')	.removeClass('is-visible');
+			$('.navigation__menu')  .removeClass('is-visible');
 		}
 	});
+
+	//scrollTo
+	$('.menu__link').click(function (i) {
+
+		i.preventDefault();
+		var idscroll = $(this).attr('href');
+		$.scrollTo(idscroll, 800);
+		console.log(idscroll);
+
+	});
+
+	//calendar
+	$('.calendar').fullCalendar({
+		header: {
+			left: 'prev,next title',
+			center: '',
+			right: ''
+		},
+		eventLimit: true,
+		eventRender: function(event, element){
+			element.closest('.is-event').find(".fc-content").append('<img class="event__image" src="img/photos/news2.jpg" alt="news">');
+			element.closest('.event-another').find(".fc-content").append('<img class="event__image" src="img/photos/attack.jpg" alt="attack">');
+		},
+		events: [
+			{
+				className: 'is-event',
+				title: '',
+				start: '2015-04-10',
+				allDay: true
+			},
+			{
+				className: 'event-another',
+				title: '',
+				start: '2015-04-20',
+				allDay: true
+			},
+		]
+	 });
 
 });
